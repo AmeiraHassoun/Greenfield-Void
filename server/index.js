d7d755b79fac0bacfb1c5dbb47dca5ecec5b1ab4
AmeiraHassoun <36267112+AmeiraHassoun@users.noreply.github.com>
2018-04-30 16:02:16 +0300

    Revert "one to one refresh"

--- server/index.js
+++ server/index.js
@@ -26,7 +26,6 @@ app.use(bodyParser.json())
 
 var curRoom= 'Public'
 var to ='';
-var privateMsg=[];
 
 
 app.use(session({
@@ -68,23 +67,27 @@ app.get('/messages',function(req, res){
 	var result=[]
 	db.Room.findOne({roomname:curRoom}, function(err,data){
 		if(data === null){
+			console.log(data)
+			db.User.findOne({user:req.session.user.user},function(err,user){
+				console.log(user.private)
+				for(var i=0;i<user.private.length;i++){
+					if(to === user.private[i].to){
+						for(var j=0;j<user.private[i].message.length;j++){
+							result.push({user:req.session.user.user,msg:user.private[i].message[j]})
+						}
+					}	
+				}	
+			})
 
-			
-			console.log(privateMsg)
-			res.send(privateMsg)
-		}
-	
-		else{
-
+		}else{
 			for(var i=0;i<data.messages.length;i++){
 				result.push({user:data.messages[i].username,msg:data.messages[i].message})
 			}
-			res.send(result)
 
 		}
 		
-		//console.log(result)
-		
+		console.log(result)
+		res.send(result)
 	})
 
 })
@@ -347,18 +350,14 @@ socket.on('new_msg',function(data){
 				for(var i=0;i<user1.private.length;i++){
 					if(user1.private[i].to === room){
 						user1.private[i].message.push(message)
-						privateMsg.push({user:user1.user,msg:message})
-
 					}
 				}
-				db.save(user1    )
+				db.save(user1)
 
 				db.User.findOne({user:to},function(err,user2){
 					for(var i=0;i<user2.private.length;i++){
 						if(user2.private[i].to === data.username ){
 							user2.private[i].message.push(message)
-						privateMsg.push({user:user2.user,msg:message})
-
 						}
 					}
 				db.save(user2)
@@ -369,11 +368,10 @@ socket.on('new_msg',function(data){
 			}
 			else{
 				db.Room.findOne({roomname:room},function(err,room1){
-					if(room1 !== null){
 
 					room1.messages.push({'username':user, 'message':message})
 					db.saveRoom(room1)
-}
+
 				})
 			}
 		})
